c615af8e8c04449b88691ebbe983d2128a275e3c
Dmitry Derevitsky <d_der@mail.ru>
2018-06-11 23:20:15 +0300

    Updated

--- src/main/java/by/my/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package by.my;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig {
-}
